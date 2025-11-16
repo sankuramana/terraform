a51f76b4f8b472da5c684eff8f998d223e948dea
ramana sanku <sankuramana456@gmail.com>
2025-11-16 18:28:36 +0530

    this is roboshop

---
+++ forloop/ec2.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "terraform" {
+  for_each = var.instances
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = each.value                            #"t3.micro"== each.vale
+  vpc_security_group_ids= [aws_security_group.allow_everythig.id]
+
+
+  tags = { #map or object
+    Name = each.key                                   #"terraform"
+    Terraform= "true"
+  }
+}
+# one resource shoul be in one block even that can be simple resourec
+resource "aws_security_group" "allow_everythig" { #name of resourece== allw_everything #terraform-reffrence
+  # ... other configuration ...
+  name = "allow-evrything" ##???? #security groupname
+  ingress {
+    from_port       = 0 #from port 0 to o means all ports
+    to_port         = 0
+    protocol        = "-1" # -1 means all protocols
+    cidr_blocks =["0.0.0.0/0"] #internet
+  }
+  
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks =["0.0.0.0/0"] #ipvs
+    
+    
+  }
+  tags = {  #this name is for user display
+    Name= "allow-everythig"
+
+  }
+
+}
+
+  
